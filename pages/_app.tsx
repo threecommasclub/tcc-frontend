06d9e3b82e13f56b1863f2888a996b8e538762da
Jay Park <jay.jongcheol.park@gmail.com>
2019-08-20 19:50:46 -0400

    Setup Next & Tailwindcss

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import '../styles/tailwind.css';
+
+import React from 'react';
+import App from 'next/app';
+
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
+}
---
+++ tcc-frontend-git/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1 className="tw-bg-red-400 tw-text-white">Index.js</h1>
+  </div>
+);
+
+export default Home;
