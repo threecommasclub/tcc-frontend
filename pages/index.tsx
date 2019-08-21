1cf65faadd238c2a79ccf576ed6233520698c5d8
Jay Park <jay.jongcheol.park@gmail.com>
2019-08-20 22:52:34 -0400

    Change text in main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = (): JSX.Element => {
   return (
     <div>
-      <h1 className="tw-bg-red-400 tw-text-white">Index.js</h1>
+      <h1 className="tw-bg-red-400 tw-text-white">TCC</h1>
     </div>
   );
 };
