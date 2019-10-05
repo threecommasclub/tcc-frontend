eefcc9ed41ba8a929ed7a7861a3014013aabd931
Jay Park <jay.jongcheol.park@gmail.com>
2019-10-05 17:57:44 -0400

    Add index to components

---
+++ components/index.tsx
@@ -0,0 +1,4 @@
+export { default as Button } from './buttons/button';
+export { default as CompanyCard } from './cards/company-card';
+export { default as Header } from './header/header';
+export { default as Map } from './map/map';
