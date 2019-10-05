7d25b7391790cfa74159c15391ba94607ec72158
Jay Park <jay.jongcheol.park@gmail.com>
2019-10-05 17:15:58 -0400

    Add mapbox

---
+++ components/map/map.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
+
+type MapProps = {
+  latitude: number;
+  longitude: number;
+  width: number;
+  height: number;
+  zoom?: number;
+};
+
+const Map = ({ latitude, longitude, width, height, zoom }: MapProps) => {
+  const [viewport, setViewPort] = useState({
+    latitude,
+    longitude,
+    width,
+    height,
+    zoom,
+  });
+  return (
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={process.env.MAP_BOX_TOKEN}
+      onViewportChange={viewport => setViewPort(viewport)}
+    ></ReactMapGL>
+  );
+};
+
+Map.defaultProps = {
+  zoom: 10,
+};
+
+export default Map;
