55876f6936713924093eb52a542540bd8029a9de
jayjongcheolpark <jay.jongcheol.park@gmail.com>
2019-10-12 13:26:11 -0400

    Add viewport to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,17 @@
-import { configure, addDecorator } from '@storybook/react';
+import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 import '../styles/tailwind.css';
 
 addDecorator(withKnobs);
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+    defaultViewport: 'iphonex',
+  },
+});
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../components', true, /\.stories\.tsx$/);
