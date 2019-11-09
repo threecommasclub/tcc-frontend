d3fa654b50d9654424df35002f30b836370b8b23
sungwook lee <sungwooklee@sungwookui-MacBookPro.local>
2019-11-09 17:05:22 -0500

    Curation card added

--- components/cards/company/company-card.mobile.tsx
+++ components/cards/company/company-card.mobile.tsx
@@ -8,7 +8,7 @@ type CompanyCardProps = {
 const tags = ['Fintech', 'Employ Benefit', 'Consulting'];
 
 export const CompanyCard = ({ name, address }: CompanyCardProps) => (
-  <div className="p-5 rounded-exlg h-auto relative shadow-md border-gray-300 border">
+  <div className="p-5 rounded-exlg h-auto relative shadow-md border-gray-300 border mt-10 mb-5">
     <div className="w-1/6 h-1/6 inline-block m-0 align-middle">
       <div className="rounded-full bg-gray-600 w-16 h-16 absolute top-in20 shadow-md"></div>
     </div>
@@ -22,7 +22,7 @@ export const CompanyCard = ({ name, address }: CompanyCardProps) => (
       </div>
     </div>
     {tags.map((tag, index) => (
-      <div className="inline-block rounded-full bg-gray-500 mr-2" key={index}>
+      <div className="inline-block rounded-full bg-gray-500 mr-2 my-1" key={index}>
         <span className="font-bold text-base text-white px-4">{tag}</span>
       </div>
     ))}
--- components/cards/curation/curation-card.mobile.tsx
+++ components/cards/curation/curation-card.mobile.tsx
@@ -1,14 +1,76 @@
 import React from 'react';
+import { FlickingEvent, SelectEvent, ChangeEvent, NeedPanelEvent } from '@egjs/flicking';
+import Flicking from '@egjs/react-flicking';
 
 type CurationProps = {
   titleImage: HTMLImageElement;
   title: string;
 };
 
+const dummyData = [
+  'Top 10\nMost Impactful\nAI\nStartups in Toronto',
+  'Top 6\nFast Growing\nFintech\nStartups in New York',
+  'Top 7\nFast Growing\nFood Tech\nStartups in Vancouver',
+];
+
 export const CurationCard = ({ titleImage, title }: CurationProps) => (
-  <div className="w-full shadow p-6">
-    <div className="w-full">{title}</div>
-  </div>
+  <Flicking
+    tag="div"
+    onNeedPanel={(e: NeedPanelEvent) => {}}
+    onMoveStart={(e: FlickingEvent) => {}}
+    // onMove={e => {
+    //   const flicking = e.currentTarget;
+
+    //   flicking.getAllPanels(true).forEach(panel => {
+    //     panel.getElement().innerHTML = panel.getProgress().toFixed(2);
+    //   });
+    // }}
+    onMove={(e: FlickingEvent) => {}}
+    onMoveEnd={(e: FlickingEvent) => {}}
+    onHoldStart={(e: FlickingEvent) => {}}
+    onHoldEnd={(e: FlickingEvent) => {}}
+    onRestore={(e: FlickingEvent) => {}}
+    onSelect={(e: SelectEvent) => {}}
+    onChange={(e: ChangeEvent) => {}}
+    classPrefix="eg-flick"
+    deceleration={0.0075}
+    horizontal={true}
+    circular={false}
+    infinite={false}
+    infiniteThreshold={0}
+    lastIndex={Infinity}
+    threshold={40}
+    duration={100}
+    panelEffect={x => 1 - Math.pow(1 - x, 3)}
+    defaultIndex={0}
+    inputType={['touch', 'mouse']}
+    thresholdAngle={45}
+    bounce={10}
+    autoResize={true}
+    adaptive={true}
+    zIndex={2000}
+    bound={false}
+    overflow={false}
+    hanger={'30%'}
+    anchor={'50px'}
+    gap={10}
+    moveType={{ type: 'snap', count: Infinity }}
+    // moveType = {{type: "freeScroll"}}
+    collectStatistics={false}
+  >
+    {dummyData.map((data, index) => (
+      <div className="bg-orange-500 p-5 rounded-exlg w-auto" key={index}>
+        <div className="w-full">
+          {data.split('\n').map((item, i) => (
+            <span className="text-white font-bold text-lg whitespace-no-wrap overflow-hidden" key={i}>
+              {item}
+              <br />
+            </span>
+          ))}
+        </div>
+      </div>
+    ))}
+  </Flicking>
 );
 
 export default CurationCard;
